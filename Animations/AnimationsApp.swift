c76d1b5bc3a8edd950e05ee52015bbab563f8bd2
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-08-18 11:41:34 +0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Naveed on 18/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
